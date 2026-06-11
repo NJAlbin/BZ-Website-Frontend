ddf15e5d8aa21ae8ce97e534e7e45847cb9e6db6
Krishnaraj <kr2459589@gmail.com>
2026-06-11 23:45:33 +0530

    resolved warnings and errors in success singlepage, newsletter section, successstories

--- components/NewsLetter/Slide.tsx
+++ components/NewsLetter/Slide.tsx
@@ -3,7 +3,8 @@
 import React, { useState } from "react";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import Link from "next/link";
-import img from "next/image";
+import Image from "next/image";
+
 
 type News = {
   id: number;
@@ -36,7 +37,7 @@ const Slide = () => {
       category: "News",
       date: "April 16, 2024",
       readTime: "10 min read",
-      image: "newssingle1.jpg",
+      image: "/newssingle1.jpg",
     },
     {
       id: 2,
@@ -51,7 +52,7 @@ const Slide = () => {
       category: "News",
       date: "July 29, 2023",
       readTime: "10 min read",
-      image: "newssingle2.jpg",
+      image: "/newssingle2.jpg",
     },
     {
       id: 3,
@@ -68,7 +69,7 @@ const Slide = () => {
       category: "News",
       date: "November 30, 2023",
       readTime: "10 min read",
-      image: "newssingle3.jpg",
+      image: "/newssingle3.jpg",
     },
   ];
 
@@ -83,55 +84,59 @@ const Slide = () => {
         >
           {NewsData.map((news, index) => (
             <Link
-              key={news.id}
-              href={`/news/${news.slug}`}
-              className="min-w-full h-full relative flex items-end"
-            >
-              {/* img */}
-              <img
-                src={news.image}
-                className="absolute inset-0 w-full h-[300px] sm:h-[550px] object-cover rounded-2xl"
-                alt="news"
-              />
+  key={news.id}
+  href={`/news/${news.slug}`}
+  className="min-w-full h-full relative flex items-end"
+>
+  {/* img */}
+  <div className="absolute inset-0 w-full h-[300px] sm:h-[550px]">
+    <Image
+      src={news.image}
+      alt="news"
+      fill
+      className="object-cover rounded-2xl"
+      sizes="(max-width: 640px) 100vw, 100vw"
+    />
+  </div>
 
-              {/* White Card */}
-              <div
-                className={`relative mb-3 sm:mb-6 ml-3 sm:ml-15 
-                w-[80%] sm:w-[633px] 
-                px-3 py-4 sm:px-8 sm:py-10
-                bg-white rounded-2xl 
-                shadow-2xl border border-gray-200
-                ${plusJakarta.className}
-                
-                ${
-                  index === 0
-                    ? "h-[250px] sm:h-[310px]"
-                    : index === 1
-                    ? "h-[250px] sm:h-[340px]"
-                    : "h-[250px] sm:h-[390px]"
-                }
-                `}
-              >
-                {/* Category */}
-                <h1 className="flex items-center text-xs sm:text-sm font-semibold text-[rgb(73,0,167)] mb-3 sm:mb-4">
-                  <span className="w-[16px] h-[16px] bg-[rgb(73,0,167)] rounded-full mr-2"></span>
-                  <span className="text-[14px] font-normal">
-                    {news.category}
-                  </span>
-                </h1>
+  {/* White Card */}
+  <div
+    className={`relative mb-3 sm:mb-6 ml-3 sm:ml-15 
+    w-[80%] sm:w-[633px] 
+    px-3 py-4 sm:px-8 sm:py-10
+    bg-white rounded-2xl 
+    shadow-2xl border border-gray-200
+    ${plusJakarta.className}
+    
+    ${
+      index === 0
+        ? "h-[250px] sm:h-[310px]"
+        : index === 1
+        ? "h-[250px] sm:h-[340px]"
+        : "h-[250px] sm:h-[390px]"
+    }
+    `}
+  >
+    {/* Category */}
+    <h1 className="flex items-center text-xs sm:text-sm font-semibold text-[rgb(73,0,167)] mb-3 sm:mb-4">
+      <span className="w-[16px] h-[16px] bg-[rgb(73,0,167)] rounded-full mr-2"></span>
+      <span className="text-[14px] font-normal">
+        {news.category}
+      </span>
+    </h1>
 
-                {/* Title */}
-                <h1 className="text-lg sm:text-4xl text-black leading-tight mb-3 sm:mb-5 font-semibold">
-                  {news.title}
-                </h1>
+    {/* Title */}
+    <h1 className="text-lg sm:text-4xl text-black leading-tight mb-3 sm:mb-5 font-semibold">
+      {news.title}
+    </h1>
 
-                {/* Meta */}
-                <span className="text-black font-medium text-xs sm:text-sm">
-                  {news.date}
-                  <span className="ml-6"> • {news.readTime}</span>
-                </span>
-              </div>
-            </Link>
+    {/* Meta */}
+    <span className="text-black font-medium text-xs sm:text-sm">
+      {news.date}
+      <span className="ml-6"> • {news.readTime}</span>
+    </span>
+  </div>
+</Link>
           ))}
         </div>
       </div>
